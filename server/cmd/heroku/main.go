bcf064bfa8b382e846b44708288a0ffe145cb3ac
kevindoubleu <54675998+kevindoubleu@users.noreply.github.com>
2022-04-04 18:04:04 +0700

    extend db operation timeout

--- server/cmd/heroku/main.go
+++ server/cmd/heroku/main.go
@@ -24,7 +24,7 @@ func Start() {
 
 	fmt.Println("db initializec")
 
-	dbOperation, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	dbOperation, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 	cursor, err := db.Collection("questions").Find(dbOperation, bson.M{})
 	if err != nil {
